dafe0b2b1cf17ccf9233700b00da449cebd53c87
unknown <vicccccc@gmail.com>
2019-05-10 19:13:49 +0300

    3 pahare

---
+++ Inversare.java
@@ -0,0 +1,12 @@
+package Vineri10mai;
+
+public class Inversare {
+    public static void main(String[] args) {
+        String pahar1 = "BERE";
+        String pahar2 = "VIN";
+        System.out.println(pahar1 + " " + pahar2);
+
+
+    }
+
+}
